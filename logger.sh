0081c098a376ebc9c3178535ffe1210ae967023e
elhousseine lakhfif <elakhfif@c1r6p6.1337.ma>
2024-09-17 10:28:21 +0100

    logtime added

--- logger.sh
+++ logger.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-python -m pip install requests
+python3 -m venv myenv > /dev/null
+source myenv/bin/activate > /dev/null
+pip install requests > /dev/null
+
 
 if [ ! -d "$HOME/.logtime" ]; then
     mkdir $HOME/.logtime
@@ -9,3 +12,7 @@ fi
 if [ ! -f "$HOME/.logtime/main.py" ]; then
     cp  main.py $HOME/.logtime/main.py
 fi
+
+deactivate
+
+rm -rf myenv
